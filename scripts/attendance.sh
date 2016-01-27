4efb2dc3bf63ed07a3453b2706b34ecaee4ed319
tkykhnzw <hnzw.tkyk@gmail.com>
2016-01-27 10:37:45 +0900

    making attendance.sh

--- scripts/attendance.sh
+++ scripts/attendance.sh
@@ -1,7 +1,26 @@
 #/bin/sh
+# 勤怠記録をつけるスクリプト
 
-if [ -e $HOME/Dropbox/attendance/hoge.csv ]; then
-    echo 'aru'
+DIR=${HOME}/Dropbox/attendance
+TODAY=`date "+%Y/%m/%d"`
+THIS_MONTH=`date "+%Y%m"`
+FILE_PATH=${DIR}/${THIS_MONTH}.csv
+
+saveAttendance(){
+    grep -E ${TODAY} ${FILE_PATH}
+    if [ $? -eq 0 ]; then
+    else
+    fi
+    echo 'complete'
+}
+
+echo 'fucking hello hananana!!'
+echo "today is ${TODAY}"
+
+if [ -e ${FILE_PATH} ]; then
+    saveAttendance
 else
+    touch ${FILE_PATH}
     echo 'nai'
 fi
+
