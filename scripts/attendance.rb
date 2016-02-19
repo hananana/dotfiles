047030c75c5a9415bc6e7144d81ee0d71dae627e
hananana <hnzw.tkyk@gmail.com>
2016-02-19 20:19:56 +0900

    wip attendance.rb

---
+++ scripts/attendance.rb
@@ -0,0 +1,101 @@
+#ruby
+
+require 'csv'
+require 'fileutils'
+
+dir = ENV['HOME'] + '/Dropbox/attendance/'
+now = Time.now
+file_name = now.year.to_s + sprintf("%02d", now.month) + '.csv'
+file_path = dir + file_name
+
+def formattedToday()
+  now = Time.now
+  now.year.to_s + '/' + now.month.to_s + '/' + now.day.to_s
+end
+
+def formattedNow()
+  now = Time.now
+  now.hour.to_s + ':' + now.min.to_s
+end
+
+def overwrite(path, csv)
+  File.open(path, 'w') do |file|
+    csv.each do |row|
+      file.write(row[0] + ',')
+      file.write(row[1] + ',')
+      file.write(row[2] + '\n')
+    end
+  end
+end
+
+def existToday(csv)
+  now = Time.now
+  dates = csv.map{|row| row[0]}
+  flag = false
+  dates.each do |date|
+    year =  date.split('/')[0].to_i
+    month =  date.split('/')[1].to_i
+    day =  date.split('/')[2].to_i
+    if year == now.year && month == now.month && day == now.day then
+      flag = true
+    end
+  end
+  return flag
+end
+
+def todayHelloTime(csv)
+  now = Time.now
+  csv.length.times do |i|
+    date = csv[i][0]
+    year =  date.split('/')[0].to_i
+    month =  date.split('/')[1].to_i
+    day =  date.split('/')[2].to_i
+    if year == now.year && month == now.month && day == now.day then
+      return csv[i][1]
+    end
+  end
+end
+
+def printInfo(csv)
+  p '出勤日数: ' + csv.length.to_s
+
+  totalHour = 0
+  totalSec = 0
+  csv.each do |row|
+    helloStrings = row[1].split(':')
+    byeStrings = row[2].split(':')
+    tempHello = Time.local(2000,1,1,helloStrings[0].to_i,helloStrings[1].to_i,0)
+    tempBye = Time.local(2000,1,1,byeStrings[0].to_i,byeStrings[1].to_i,0)
+    totalSec += tempBye - tempHello - 60 * 60
+    totalHour = totalSec/60/60
+  end
+  p '働いてる時間: ' + totalHour.to_s
+
+  baseTime = 60 * 60 * 8 * csv.length
+  diff = (totalSec - baseTime) / 60 / 60
+  p '残業時間: ' + diff.to_s
+end
+
+def logAttendance(path, flag)
+  csv = CSV.read(path)
+  if flag == 'hello' then
+    if existToday(csv) then
+      csv.delete_at(csv.length - 1)
+    end
+    csv.push([formattedToday() + ',', formattedNow() + ',', '20:00'])
+  else
+    t = todayHelloTime(csv)
+    if existToday(csv) then
+      csv.delete_at(csv.length - 1)
+    end
+    csv.push([formattedToday() + ',', t + ',', formattedNow()])
+  end
+  overwrite(path, csv)
+  printInfo(csv)
+end
+
+
+if !File.exist?(file_path) then
+  FileUtils.touch(file_path)
+end
+logAttendance(file_path, ARGV[0])
