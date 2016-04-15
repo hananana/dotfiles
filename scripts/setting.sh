a1f296204858d8bc66ef254930825134dc63f615
hananana <hnzw.tkyk@gmail.com>
2016-04-15 10:21:14 +0900

    change zplug setting

--- scripts/setting.sh
+++ scripts/setting.sh
@@ -68,6 +68,13 @@ if [ -e ~/.tigrc ]; then
 fi
 ln -s ~/$DOTFILES_DIR/.tigrc ~/.tigrc
 
+# zplug
+if [ -e ~/.zplug ]; then
+    echo "zplug already installed"
+else
+    git clone https://github.com/b4b4r07/zplug ~/.zplug
+fi
+
 # neobundle
 if [ ! -e ~/.vim/bundle ]; then
     mkdir ~/.vim/bundle
