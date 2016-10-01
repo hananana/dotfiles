cf730180d1d4338f4f41a79e0c585351179268f7
hananana <hnzw.tkyk@gmail.com>
2016-10-02 01:28:11 +0900

    update README and setting

--- README.md
+++ README.md
@@ -4,50 +4,17 @@ C++,C#,ruby,markdown,json,swift,go,pythonに対応済み
 
 参考のakinama氏の[dotfiles](https://github.com/akinama/dotfiles)  
 
-## 注意
-monoのverは4.0.1じゃないとyoucompletemeが動かない罠
-
 ## 手順
+
+- brewをいれる
 
 ```sh
 $ git clone git@github.com:hanana0501/dotfiles.git ~/.dotfiles
 $ .dotfiles/scripts/setting.sh
 ```
-
-### karabiner
-
-```sh
-$ brew install caskroom/cask/brew-cask
-$ brew cask install karabiner
-$ cd ~/Library/Application\ Support/Karabiner
-$ rm private.xml
-$ ln -s ~/.dotfiles/dotfiles/private.xml private.xml
-```
-
-karabinerを起動してLeave Insert Mode（以下略をONに
-
-### tmux
-```sh
-$ gem install tmuxinator
-```
+- karabinerを起動してLeave Insert Mode（以下略をONに
 
 ### terminal setting
 
-- inconsolata 12pt
+- Menleo 12pt
 - Japonesque
-
-## tools(いつか自動化したいンゴ)
-
-unity
-xcode
-f.lux
-dropbox
-androidstudio
-sourcetree
-wallcat
-pushbullet
-
-eclim
-brew cask install eclipse-java
-http://eclim.org/install.html
-java -jar /path/to/eclimhoge.jar
--- scripts/setting.sh
+++ scripts/setting.sh
@@ -27,74 +27,79 @@ confirm_brew()
     done
 }
 
+set_symlink()
+{
+    # symlink
+    ## gitconfig
+    if [ -e ~/.gitconfig ]; then
+        rm -f ~/.gitconfig
+    fi
+    ln -s ~/$DOTFILES_DIR/.gitconfig ~/.gitconfig
+
+    ## gitglobal
+    if [ -e ~/.gitignore_global ]; then
+        rm -f ~/.gitignore_global
+    fi
+    ln -s ~/$DOTFILES_DIR/.gitignore_global ~/.gitignore_global
+
+    ## zshrc
+    if [ -e ~/.zshrc ]; then
+        rm -f ~/.zshrc
+    fi
+    ln -s ~/$DOTFILES_DIR/.zshrc ~/.zshrc
+
+    ## vimrc
+    if [ -e ~/.vimrc ]; then
+        rm -f ~/.vimrc
+    fi
+    ln -s ~/$DOTFILES_DIR/.vimrc ~/.vimrc
+
+    ## gvimrc
+    if [ -e ~/.gvimrc ]; then
+        rm -f ~/.gvimrc
+    fi
+    ln -s ~/$DOTFILES_DIR/.gvimrc ~/.gvimrc
+
+    ## xvimrc
+    if [ -e ~/.xvimrc ]; then
+        rm -f ~/.xvimrc
+    fi
+    ln -s ~/$DOTFILES_DIR/.xvimrc ~/.xvimrc
+
+    ## vim directory
+    if [ -e ~/.vim ]; then
+        rm -rf ~/.vim
+    fi
+    ln -s ~/$DOTFILES_DIR/.vim ~/.vim
+
+    ## tmux
+    if [ -e ~/.tmux.conf ]; then
+        rm -rf ~/.tmux.conf
+    fi
+    ln -s ~/$DOTFILES_DIR/.tmux.conf ~/.tmux.conf
+
+    ## tmuxinator
+    if [ -e ~/.tmuxinator ]; then
+        rm -rf ~/.tmuxinator
+    fi
+    ln -s ~/$DOTFILES_DIR/.tmuxinator ~/.tmuxinator
+
+    ## ctags
+    if [ -e ~/.ctags ]; then
+        rm -rf ~/.ctags
+    fi
+    ln -s ~/$DOTFILES_DIR/.ctags ~/.ctags
+
+    ## tigrc
+    if [ -e ~/.tigrc ]; then
+        rm -rf ~/.tigrc
+    fi
+    ln -s ~/$DOTFILES_DIR/.tigrc ~/.tigrc
+}
+
 confirm_brew
+set_symlink
 
-# symlink
-## gitconfig
-if [ -e ~/.gitconfig ]; then
-    rm -f ~/.gitconfig
-fi
-ln -s ~/$DOTFILES_DIR/.gitconfig ~/.gitconfig
-
-## gitglobal
-if [ -e ~/.gitignore_global ]; then
-    rm -f ~/.gitignore_global
-fi
-ln -s ~/$DOTFILES_DIR/.gitignore_global ~/.gitignore_global
-
-## zshrc
-if [ -e ~/.zshrc ]; then
-    rm -f ~/.zshrc
-fi
-ln -s ~/$DOTFILES_DIR/.zshrc ~/.zshrc
-
-## vimrc
-if [ -e ~/.vimrc ]; then
-    rm -f ~/.vimrc
-fi
-ln -s ~/$DOTFILES_DIR/.vimrc ~/.vimrc
-
-## gvimrc
-if [ -e ~/.gvimrc ]; then
-    rm -f ~/.gvimrc
-fi
-ln -s ~/$DOTFILES_DIR/.gvimrc ~/.gvimrc
-
-## xvimrc
-if [ -e ~/.xvimrc ]; then
-    rm -f ~/.xvimrc
-fi
-ln -s ~/$DOTFILES_DIR/.xvimrc ~/.xvimrc
-
-## vim directory
-if [ -e ~/.vim ]; then
-    rm -rf ~/.vim
-fi
-ln -s ~/$DOTFILES_DIR/.vim ~/.vim
-
-## tmux
-if [ -e ~/.tmux.conf ]; then
-    rm -rf ~/.tmux.conf
-fi
-ln -s ~/$DOTFILES_DIR/.tmux.conf ~/.tmux.conf
-
-## tmuxinator
-if [ -e ~/.tmuxinator ]; then
-    rm -rf ~/.tmuxinator
-fi
-ln -s ~/$DOTFILES_DIR/.tmuxinator ~/.tmuxinator
-
-## ctags
-if [ -e ~/.ctags ]; then
-    rm -rf ~/.ctags
-fi
-ln -s ~/$DOTFILES_DIR/.ctags ~/.ctags
-
-## tigrc
-if [ -e ~/.tigrc ]; then
-    rm -rf ~/.tigrc
-fi
-ln -s ~/$DOTFILES_DIR/.tigrc ~/.tigrc
 
 ## zplug
 if [ -e ~/.zplug ]; then
@@ -102,3 +107,10 @@ if [ -e ~/.zplug ]; then
 else
     git clone https://github.com/b4b4r07/zplug ~/.zplug
 fi
+
+## karabiner
+rm ~/Library/Application\ Support/Karabiner/private.xml
+ln -s ~/.dotfiles/dotfiles/private.xml private.xml
+
+## tmuxinator
+gem install tmuxinator
