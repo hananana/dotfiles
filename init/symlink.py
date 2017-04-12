db10bae163de823eb7a507858b8fa4c046eb2fb8
hananana <hananana.vimmer@gmail.com>
2017-04-12 22:31:02 +0900

    :hammer: change files in src name

--- init/symlink.py
+++ init/symlink.py
@@ -2,9 +2,29 @@
 # coding: UTF-8
 
 import os.path
-import logging
-import glob
+import shutil
 
 home = os.environ['HOME']
 entityDir = os.path.join(home, '.dotfiles/src')
 listDir = os.listdir(entityDir)
+listDir.remove('.DS_Store')
+
+for path in listDir:
+    dotPath = '.' + path
+    linkPath = os.path.join(home, dotPath)
+    print(linkPath)
+    entityPath = os.path.join(entityDir, path)
+    print(entityPath)
+    if os.path.exists(linkPath):
+        if os.path.isdir(linkPath):
+            if os.path.islink(linkPath):
+                os.unlink(linkPath)
+            else:
+                shutil.rmtree(linkPath)
+        else:
+            if os.path.islink(linkPath):
+                os.unlink(linkPath)
+            else:
+                os.remove(linkPath)
+
+    os.symlink(entityPath, linkPath)
