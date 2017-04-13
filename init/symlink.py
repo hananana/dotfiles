30a64690c76d78c9bc4ec625068283fc58bdcdfb
hananana <hananana.vimmer@gmail.com>
2017-04-13 09:20:47 +0900

    :hammer: resurrect symlink.py

---
+++ init/symlink.py
@@ -0,0 +1,26 @@
+#!/usr/local/bin/python
+# coding: UTF-8
+
+import os.path
+import shutil
+
+home = os.environ['HOME']
+entityDir = os.path.join(home, '.dotfiles/src')
+listDir = os.listdir(entityDir)
+
+for path in listDir:
+    dotPath = '.' + path
+    linkPath = os.path.join(home, dotPath)
+    entityPath = os.path.join(entityDir, path)
+
+    if os.path.exists(linkPath):
+	if os.path.islink(linkPath):
+	    os.unlink(linkPath)
+	elif os.path.isdir:
+	    shutil.rmtree(linkPath)
+	else:
+	    os.remove(linkPath)
+
+    print(entityPath)
+    print(linkPath)
+    os.symlink(entityPath, linkPath)
