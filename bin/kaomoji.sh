f7dc521ed6d7ff31fc5bdc4cb7b69f9896ace21f
hananana <hananana.vimmer@gmail.com>
2017-05-31 06:32:20 +0900

    :hammer: update kaomoji.sh

--- bin/kaomoji.sh
+++ bin/kaomoji.sh
@@ -1,15 +1,15 @@
 #! /bin/bash
 
-dateInt=`date '+%s'`
+dateInt=$(date '+%s')
 mod=$(($dateInt % 5))
 if [ 0 -eq $mod ]; then
-    echo "    旦_(・ω ・)"
+    echo '    旦_(´･ω･`)'
 elif [ 1 -eq $mod ]; then
-    echo "   ~旦_(・ω ・)"
+    echo '   ~旦_(´･ω･`)'
 elif [ 2 -eq $mod ]; then
-    echo "  ~~旦_(・ω ・)"
+    echo '  ~~旦_(´･ω･`)'
 elif [ 3 -eq $mod ]; then
-    echo " ~~~旦_(・ω ・)"
+    echo ' ~~~旦_(´･ω･`)'
 elif [ 4 -eq $mod ]; then
-    echo "~~~~旦_(・ω ・)"
+    echo '~~~~旦_(´･ω･`)'
 fi
