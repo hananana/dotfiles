a1ceac95c9d105438cd44e4995e2938140e87579
hananana <hananana.vimmer@gmail.com>
2017-07-18 10:55:10 +0900

    :new: add setting of karabiner-elements

---
+++ init/karabiner-elements.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+target=$HOME/.config/karabiner/assets/complex_modifications/FromEscapeToJapanese_eisu.json
+entity=$HOME/.dotfiles/karabiner-elements/FromEscapeToJapanese_eisu.json
+
+ln -s "$entity" "$target"
