3af65dcc10ec8b7d86a69ad1b25d277844e70a99
hananana <hananana.vimmer@gmail.com>
2017-12-13 08:37:12 +0900

    :hammer: add alias in config.fish

--- src/config.fish
+++ src/config.fish
@@ -1,9 +1,20 @@
 
 if type -q balias
     balias gst 'git status'
+    balias gf 'git fetch --prune'
+    balias gfp 'git fetch --prune'
+    balias ga 'git add'
+    balias gaa 'git add --all'
+    balias gb 'git branch'
+    balias gba 'git branch -a'
+    balias gc 'git commit -v'
+    balias gcb 'git checkout -b'
+    balias gco 'git checkout'
+    balias gd 'git diff'
+    balias gl 'git pull --prune'
     balias l 'ls -la'
 else
-    echo 'install balias'
+    echo 'install balias. fisher balias'
 end
 
 function cdz
