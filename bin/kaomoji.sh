35d6dd0b681151b8e18cc87c5698e3131e0a30fe
hananana <hananana.vimmer@gmail.com>
2017-12-20 08:04:28 +0900

    :hammer: update misc

--- bin/kaomoji.sh
+++ bin/kaomoji.sh
@@ -3,13 +3,13 @@
 dateInt=$(date '+%s')
 mod=$(($dateInt % 5))
 if [ 0 -eq $mod ]; then
-    echo '    旦_(´･ω･`)'
+    echo '  (´･ω ･`)  '
 elif [ 1 -eq $mod ]; then
-    echo '   ~旦_(´･ω･`)'
+    echo '  (´･ω ･`)  '
 elif [ 2 -eq $mod ]; then
-    echo '  ~~旦_(´･ω･`)'
+    echo '  (´･ω ･`)  '
 elif [ 3 -eq $mod ]; then
-    echo ' ~~~旦_(´･ω･`)'
+    echo '  (´･ω ･`)  '
 elif [ 4 -eq $mod ]; then
-    echo '~~~~旦_(´･ω･`)'
+    echo '  (´･ω ･`)b '
 fi
