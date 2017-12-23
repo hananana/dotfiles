099e1b66dc0dec4efb7be569c0356fce1c15cce8
hananana <hananana.vimmer@gmail.com>
2017-12-23 11:27:19 +0900

    :hammer: add ntfy in requirements3.txt

--- src/init/karabiner-elements.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-target=$HOME/.config/karabiner/assets/complex_modifications/FromEscapeToJapanese_eisu.json
-entity=$HOME/.dotfiles/karabiner-elements/FromEscapeToJapanese_eisu.json
-
-ln -s "$entity" "$target"
