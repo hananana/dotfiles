3884d06ec0f83122900962e9e232f95cf99958e3
hananana <hananana.vimmer@gmail.com>
2019-04-25 08:22:24 +0900

    :new: add powershell profile

---
+++ src/Microsoft.PowerShell_profile.ps1
@@ -0,0 +1,8 @@
+﻿
+Import-Module 'C:\tools\poshgit\dahlbyk-posh-git-a4faccd\src\posh-git.psd1'
+
+# Chocolatey profile
+$ChocolateyProfile = "$env:ChocolateyInstall\helpers\chocolateyProfile.psm1"
+if (Test-Path($ChocolateyProfile)) {
+  Import-Module "$ChocolateyProfile"
+}
